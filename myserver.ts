49c4d8ed7940d69b909d6ed7926a60f32a53d481
alliefitz22 <95373583+alliefitz22@users.noreply.github.com>
2023-04-13 11:52:02 -0400

    added cors

--- myserver.ts
+++ myserver.ts
@@ -1,6 +1,8 @@
 import express, {Application, Request, Response} from "express";
+import cors from "cors"
 
 const app: Application = express()
+app.use(cors())
 const PORT = process.env.PORT ?? 8000;
 const wordArray = ["apple", "shade", "watch", "agree", "power", "piece", "bloom", "plead", "sorry", "spoon", "bunny", "pause", "audio", "dream"]
 //Define GET endpoint(s)
